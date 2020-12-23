3d18a1a947b95cad03504bdb5969b08b097b4b58
Ashish Chauhan <imashish58@gmail.com>
2020-12-24 00:28:26 +0530

    init

--- lib/splash.dart
+++ lib/splash.dart
@@ -34,10 +34,10 @@ class SplashState extends State<Splash>{
     );
     
     Timer(Duration(seconds: 3),
-            ()=>{
-                  Navigator.pop(context),
-                  Navigator.pushNamed(context, Dashboard.RouteName)
-    }
+      ()=>{
+            Navigator.pop(context),
+            Navigator.pushNamed(context, SelectLanguage.RouteName)
+      }
     );
   }
   @override
