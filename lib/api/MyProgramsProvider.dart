2954c31c4b5eba7a89fdad73e0b9e8ff157a5b29
ashimkansal <kansal.136@gmail.com>
2020-12-31 00:12:22 +0530

    assessment_form.dart issue fixed

--- lib/api/MyProgramsProvider.dart
+++ lib/api/MyProgramsProvider.dart
@@ -3,8 +3,10 @@ import 'dart:io';
 
 import 'package:flutter_app/common/SharedPreferences.dart';
 import 'package:flutter_app/model/BuyPlanResponse.dart';
+import 'package:flutter_app/model/CancelPlanResponse.dart';
 import 'package:flutter_app/model/GetProgramsResponse.dart';
 import 'package:flutter_app/model/AuthModel.dart';
+import 'package:flutter_app/model/MyPlanResponse.dart';
 import 'package:flutter_app/utils/allstrings.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,6 +23,31 @@ import 'package:shared_preferences/shared_preferences.dart';
         });
     print(response.body);
     return getProgramsResponseFromJson(response.body);
+
+  }
+
+  Future<MyPlanResponse> getMyPrograms() async {
+    var token = await GetStringToSP("token");
+
+    final response = await http.get(url+"/myProgram",
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+          'token' : token
+        });
+    print(response.body);
+    return myPlanResponseFromJson(response.body);
+
+  }
+
+  Future<CancelPlanResponse> cancelPrograms() async {
+    var token = await GetStringToSP("token");
+    final response = await http.get(url+"/cancelProgram",
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+          'token' : token
+        });
+    print(response.body);
+    return cancelPlanResponseFromJson(response.body);
 
   }
 
