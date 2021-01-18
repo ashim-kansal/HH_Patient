6b9817c6651c94516d08e5208077c5b889b57287
Ashish Chauhan <imashish58@gmail.com>
2021-01-19 00:55:48 +0530

    fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -17,6 +17,29 @@ import flutter_voip_push_notification
 //      [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
 //    }
     
+    // Setup Notifications
+
+//   if ([UNUserNotificationCenter class] != nil) {
+//     // iOS 10 or later
+//     // For iOS 10 display notification (sent via APNS)
+//     [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+//     UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+//     UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+//     [FIRMessaging messaging].delegate = self;
+//     [[UNUserNotificationCenter currentNotificationCenter]
+//      requestAuthorizationWithOptions:authOptions
+//      completionHandler:^(BOOL granted, NSError * _Nullable error) {
+//        if (error) { NSLog(@"%@", error); }
+//      }];
+//   } else {
+//     // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
+//     UIUserNotificationType allNotificationTypes =
+//     (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+//     UIUserNotificationSettings *settings =
+//     [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+//     [application registerUserNotificationSettings:settings];
+//   }
+    
     if #available(iOS 10.0, *) {
       // For iOS 10 display notification (sent via APNS)
       UNUserNotificationCenter.current().delegate = self
